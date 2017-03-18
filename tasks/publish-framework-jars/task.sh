4000a098a1a926ea2786ac2ca6b1c645b8403ac0
patrickcrocker <patrick.crocker@gmail.com>
2017-03-18 07:30:26 -0500

    fixed typo

--- tasks/publish-framework-jars/task.sh
+++ tasks/publish-framework-jars/task.sh
@@ -10,6 +10,6 @@ args=""
 [ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
 [ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
 
-cd aviato-profile
+cd framework-git
 ./mvnw install $args
 cd ..

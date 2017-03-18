8d2f1406ffc6a597728832c92126cfb182541748
patrickcrocker <patrick.crocker@gmail.com>
2017-03-18 09:16:21 -0500

    .

--- tasks/publish-framework-jars/task.sh
+++ tasks/publish-framework-jars/task.sh
@@ -2,19 +2,20 @@
 
 set -e
 
-version=$(cat framework-version/version)
-[ -f "framework-snapshot/snapshot" ] version="$version-SNAPSHOT"
+cd framework-git
 
 args=""
 [ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
 [ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
 [ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
 
-cd framework-git
+pomVersion=$(./mvnw help:evaluate -Dexpression=project.version | grep -e '^[^\[]')
+
 ./mvnw install $args
+
 cd ..
 
-cp -R ~/.m2/repository/com/example/framework/$version/* publish-jars-output/.
+cp -R ~/.m2/repository/com/example/framework/$pomVersion/* publish-jars-output/.
 
 ls -al publish-jars-output
 

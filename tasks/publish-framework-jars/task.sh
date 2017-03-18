5c18860c67e621bd222d9c71e690e7c3738cc875
patrickcrocker <patrick.crocker@gmail.com>
2017-03-18 09:24:44 -0500

    .

--- tasks/publish-framework-jars/task.sh
+++ tasks/publish-framework-jars/task.sh
@@ -9,7 +9,7 @@ args=""
 [ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
 [ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
 
-pomVersion=$(./mvnw help:evaluate -Dexpression=project.version | grep -e '^[^\[]')
+pomVersion=$(./mvnw help:evaluate -Dexpression=project.version 2>/dev/null | grep -e '^[^\[]')
 
 echo "pomVersion=$pomVersion"
 #./mvnw install $args

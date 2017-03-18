12b9aedbd2ee62c1580e1f1d7f17a483299b88c6
patrickcrocker <patrick.crocker@gmail.com>
2017-03-18 06:58:53 -0500

    first commit

---
+++ tasks/publish-framework-jars/tash.sh
@@ -0,0 +1,15 @@
+#!/bin/ash
+
+set -e
+
+#version=$(cat version/version)
+
+#args="-DversionNumber=$version"
+args=""
+[ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
+[ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
+[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
+
+cd aviato-profile
+./mvnw install $args
+cd ..
